fcd79c42bbc62676d6907f7d099904ef04da55b5
Kritarth D Kambli <kritarthkambli@gmail.com>
2022-03-18 19:29:55 +0530

    class for heart disease created

--- medblog/medappblog/models.py
+++ medblog/medappblog/models.py
@@ -6,3 +6,15 @@ from django.contrib.auth.models import User
 class Disease(models.Model):
     disease_name = models.CharField(max_length=255) #13:30
     
+class HDSymptom(models.Model):
+    age = models.IntegerField
+    anaemia = models.CharField
+    CreatiPhosph = models.IntegerField
+    anaemia = models.CharField
+    EjectFract = models.IntegerField
+    highBP = models.CharField
+    SerCreat = models.IntegerField
+    SerSodiu = models.IntegerField
+    sex = models.CharField
+    smoking = models.CharField
+    followUpTime = models.IntegerField
