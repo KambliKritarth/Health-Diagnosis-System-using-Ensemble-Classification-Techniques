048d1cd6db35fa4f7469e95354dcc362066c45f3
Kritarth D Kambli <kritarthkambli@gmail.com>
2022-03-21 06:06:02 +0530

    hdsymptom model created/ urls.py updated

--- medblog/medappblog/urls.py
+++ medblog/medappblog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('covid',views.covid, name = "covid"),
     path('heartdisease',views.heartdisease, name = "heartdisease"),
     path('diabetes',views.diabetes, name = "diabetes"),
+    path('submitmyform',views.submitmyform,name = 'submitmyform'),
 ]
