0b30185931e56fb1c4791f1215e740cec69c4507
Kritarth D Kambli <kritarthkambli@gmail.com>
2022-03-29 20:05:03 +0530

    views.py 3

--- medblog/medappblog/views.py
+++ medblog/medappblog/views.py
@@ -15,11 +15,11 @@ def heartdisease(request):
 def diabetes(request):
     return render(request,'diabetes.html',{})
 
-def getPredictions(age, anaemia, CreatiPhosph, anaemia, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime):
+def getPredictions(age, anaemia, CreatiPhosph, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime):
     
     model = pickle.load(open("careerguide.sav","rb"))
     scaled = pickle.load(open("careerguidescaler.sav","rb"))
-    prediction = model.predict(scaled.transform([age, anaemia, CreatiPhosph, anaemia, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime]))
+    prediction = model.predict(scaled.transform([age, anaemia, CreatiPhosph, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime]))
     return prediction
 
 def submitmyform(request):
@@ -29,9 +29,10 @@ def submitmyform(request):
     
     #print(mydictionary["name"]), name = mydictionary["name"], lastname = mydictionary["lastname"],
     result =getPredictions(request.POST.get('age',0) , request.POST.get('anaemia',0), request.POST.get('EjectFract',0) , request.POST.get('highBP',0) ,request.POST.get('SerCreat',0) , request.POST.get('SerSodiu',0), request.POST.get('sex',0), request.POST.get('smoking',0), request.POST.get('Sing',0), request.POST.get('followUpTime',0))
-   
-    ins = HDSymptom( result,age, anaemia, CreatiPhosph, anaemia, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime )
 
-    ins.save()
+    #ins = HDSymptom( result,age, anaemia, CreatiPhosph, EjectFract, highBP, SerCreat, SerSodiu, sex, smoking, followUpTime )
+
+    #ins.save()
+    
     #print(result)
     return render(request,'submitmyform.html',{'submitmyform':result})
